a5be14d8e5570fe1cd936f2b556744f158337640
YESHWANTH <sy185090@ncr.com>
2019-08-19 17:00:39 +0530

    webAPI

---
+++ BankAPI/Controllers/CustomerController.cs
@@ -0,0 +1,66 @@
+﻿using BankAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BankAPI.Controllers
+{
+    public class CustomerController : ApiController
+    {
+        OnlineBankEntities1 dbContext = new OnlineBankEntities1();
+
+        [HttpGet]
+        public HttpResponseMessage getAccountsOfUser(string loginId)
+        {
+            userDetail customer = dbContext.userDetails.Where(x => x.emailId == loginId).Single<userDetail>();
+            List<accountDetail> accounts = dbContext.accountDetails.Where(y => y.userId == customer.userId).ToList();
+            List<accountDetailViewModel> acc3 = new List<accountDetailViewModel>();
+
+
+            int i = 0;
+            foreach (accountDetail xyz in accounts)
+            {
+                accountDetailViewModel acc2 = new accountDetailViewModel();
+                acc2.accountBalance = xyz.accountBalance;
+                acc2.accountNumber = xyz.accountNumber;
+                acc2.accountStatus = xyz.accountStatus;
+                acc2.accountType = xyz.accountType;
+                acc2.closingDate = xyz.closingDate;
+                acc2.createdBy = xyz.createdBy;
+                acc2.createdDate = xyz.createdDate;
+                acc2.editedDate = xyz.editedDate;
+                acc2.type = xyz.type;
+                acc2.updatedBy = xyz.updatedBy;
+                acc2.userId = xyz.userId;
+                acc3.Add(acc2);
+            }
+
+            return Request.CreateResponse(HttpStatusCode.OK, accounts);            
+        }
+        [HttpGet]
+        public HttpResponseMessage getSpecificAccountOfUser(long accountNo)
+        {
+            accountDetail account = dbContext.accountDetails.Where(x => x.accountNumber == accountNo).Single<accountDetail>();
+            accountDetailViewModel acc2 = new accountDetailViewModel();
+            
+            
+            //acc2.accountBalance = account.accountBalance;
+            //acc2.accountNumber = xyz.accountNumber;
+            //acc2.accountStatus = xyz.accountStatus;
+            //acc2.accountType = xyz.accountType;
+            //acc2.closingDate = xyz.closingDate;
+            //acc2.createdBy = xyz.createdBy;
+            //acc2.createdDate = xyz.createdDate;
+            //acc2.editedDate = xyz.editedDate;
+            //acc2.type = xyz.type;
+            //acc2.updatedBy = xyz.updatedBy;
+            //acc2.userId = xyz.userId;
+            return Request.CreateResponse(HttpStatusCode.OK, account);   
+            
+
+        }
+    }
+}
---
+++ BankAPI/Controllers/LoginController.cs
@@ -0,0 +1,70 @@
+﻿using BankAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+
+namespace BankAPI.Controllers
+{
+    public class LoginController : ApiController
+    {
+        OnlineBankEntities1 dbContext = new OnlineBankEntities1();
+
+        public string Decrypt(string encrString)
+        {
+            byte[] b;
+            string decrypted;
+            try
+            {
+                b = Convert.FromBase64String(encrString);
+                decrypted = System.Text.ASCIIEncoding.ASCII.GetString(b);
+            }
+            catch (FormatException fe)
+            {
+                decrypted = "";
+            }
+            return decrypted;
+        }
+       
+        
+        [HttpGet]
+        public HttpResponseMessage checkCredentials(string loginId, string password)
+        {
+                string role;
+                try
+                {
+                    loginDetail dbObj = (loginDetail)dbContext.loginDetails.SingleOrDefault(x => x.loginId == loginId);
+                    if (dbObj == null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.NotFound,"invalid username");
+                    }
+                    else
+                    {
+                        string Dpassword = Decrypt(dbObj.loginPassword);
+                        if (password == Dpassword)
+                        {
+                            role = dbObj.userRole.ToString();
+                            return Request.CreateResponse(HttpStatusCode.OK, role);
+                        }
+                        else
+                        {
+                            return Request.CreateResponse(HttpStatusCode.NotFound,"invalid Password");
+                        }
+                    }
+                    
+                }
+                catch (Exception e)
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError,e);
+                }
+
+        }
+       
+       
+
+
+    }
+}
